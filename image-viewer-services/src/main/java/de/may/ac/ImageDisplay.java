d04e05f64709cd454a68a7124000df45f8119a89
Lieblingsmensch <Lieblingsmensch@DESKTOP-RIDFIB3.fritz.box>
2018-06-10 22:38:26 +0200

    Fix Calculation of next image

--- image-viewer-services/src/main/java/de/may/ac/ImageDisplay.java
+++ image-viewer-services/src/main/java/de/may/ac/ImageDisplay.java
@@ -9,7 +9,8 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
 import java.util.List;
 
 import javax.imageio.ImageIO;
@@ -20,10 +21,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import de.may.ac.model.ImageInfo;
-import lombok.extern.log4j.Log4j2;
 
 @Component
-@Log4j2
 public class ImageDisplay extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -50,8 +49,11 @@ public class ImageDisplay extends JFrame {
 		initUI();
 	}
 
+	/**
+	 * Init the UI with a full screen window and black background, the only
+	 * component shows the embedded buffered image
+	 */
 	private void initUI() {
-
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setUndecorated(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -74,52 +76,60 @@ public class ImageDisplay extends JFrame {
 				}
 				g.setColor(Color.black);
 				Graphics2D g2d = (Graphics2D) g;
-				g2d.drawString("imageNumber: " + imageNumber +"\r\nmsCalc: " + msCalc + "\r\nmsPassed: " + msPassed + "\r\ntime:" + time, 40, 200);
+				g2d.drawString("imageNumber: " + imageNumber + "\r\nmsCalc: " + msCalc + "\r\nmsPassed: " + msPassed
+						+ "\r\ntime:" + time, 40, 200);
 			}
 		});
 	}
 
 	@Scheduled(fixedDelay = 100)
 	public void updateImage() throws IOException {
-		long start= System.currentTimeMillis();
 		List<ImageInfo> imagesInfos = imagesController.imageInfos();
-		Calendar date = Calendar.getInstance();
-		date.set(Calendar.HOUR, 0);
-		date.set(Calendar.MINUTE, 0);
-		date.set(Calendar.SECOND, 0);
-		date.set(Calendar.MILLISECOND, 0);
-		long msPassed = System.currentTimeMillis() - date.getTimeInMillis();
-		// set to half a second so it is clear, to which second it belongs
-		long msCalc = 00;
-		int imageNumber = -1;
-		if (imagesInfos != null && !imagesInfos.isEmpty()) {
-			while (msCalc < msPassed) {
-				imageNumber = (imageNumber + 1) % imagesInfos.size();
-				ImageInfo info = imagesInfos.get(imageNumber);
-				msCalc += info.getTimeout();
-			}
-			// as we have one in buffer, imageNumber needs to be increased after calculation
-			// of current image
-			// imageNumber = (imageNumber + 1) % imagesInfos.size();
-			this.imageNumber = imageNumber;
-			this.msCalc = msCalc;
-			this.msPassed = msPassed;
-			repaint();
-			if (imgInfo == null || imgInfo.getId() != imagesInfos.get(imageNumber).getId()) {
-				imgInfo = imagesInfos.get(imageNumber);
-				repaint();
-				img = nextImg;
-				nextImg = null;
-				byte[] imageData = imagesController.image(imagesInfos.get(imageNumber).getId());
-				BufferedImage temp = ImageIO.read(new ByteArrayInputStream(imageData));
+		this.imageNumber = calcCurrentImage(imagesInfos);
+		if (imgInfo == null || imgInfo.getId() != imagesInfos.get(imageNumber).getId()) {
+			// next image needs to be loaded to Buffer, current calculated image needs to be
+			// shown
 
-				double factor = Math.min(((double) getHeight()) / temp.getHeight(),
-						((double) getWidth()) / temp.getHeight());
-				int width = (int) (temp.getWidth() * factor);
-				int height = (int) (temp.getHeight() * factor);
-				nextImg = temp.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-			}
+			// show current image, which is already in Buffer
+			imgInfo = imagesInfos.get(imageNumber);
+			img = nextImg;
+			repaint();
+
+			// load next image into Buffer
+			nextImg = null;
+			int nextImgNumber = (imageNumber + 1) % imagesInfos.size();
+			byte[] imageData = imagesController.image(imagesInfos.get(nextImgNumber).getId());
+			BufferedImage temp = ImageIO.read(new ByteArrayInputStream(imageData));
+
+			double factor = Math.min(((double) getHeight()) / temp.getHeight(),
+					((double) getWidth()) / temp.getHeight());
+			int width = (int) (temp.getWidth() * factor);
+			int height = (int) (temp.getHeight() * factor);
+			nextImg = temp.getScaledInstance(width, height, Image.SCALE_SMOOTH);
 		}
-		this.time = System.currentTimeMillis() - start;
+	}
+
+	/**
+	 * We have one image in the cache. Therefore we need to do the calculation based
+	 * on the time for the next image.
+	 * 
+	 * @return number of current image
+	 */
+	private int calcCurrentImage(List<ImageInfo> imagesInfos) {
+		long msRemaining = LocalDateTime.now().getLong(ChronoField.MILLI_OF_DAY);
+		long msOfFullPass = imagesInfos.stream()//
+				.mapToLong(ImageInfo::getTimeout)//
+				.sum();
+		msRemaining %= msOfFullPass;
+
+		for (int i = 0; i < imagesInfos.size(); i++) {
+			if (imagesInfos.get(i).getTimeout() >= msRemaining) {
+				System.out.println("Current image: " + msRemaining);
+				return i;
+			}
+			msRemaining -= imagesInfos.get(i).getTimeout();
+		}
+
+		throw new IllegalStateException("Could not determine, what the current image should be.");
 	}
 }
